56b66c784dfb3911abf6e33bbb4d4c98f724a5b2
dennee <imdennee@gmail.com>
2019-04-02 18:36:19 +0600

    add loggers factory

---
+++ src/lmp.UrlsProcessorWithProblem/LoggerFactory.cs
@@ -0,0 +1,19 @@
+﻿using NLog;
+
+namespace lmp.UrlsProcessorWithProblem
+{
+    /// <summary>
+    /// Factory for loggers creation
+    /// </summary>
+    public static class LoggerFactory
+    {
+        /// <summary>
+        /// Creates a logger
+        /// </summary>
+        /// <returns>A created logger</returns>
+        public static ILogger Create<T>()
+        {
+            return LogManager.GetLogger(typeof(T).FullName);
+        }
+    }
+}
