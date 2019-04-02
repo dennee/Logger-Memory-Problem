9093ac80aa7f8a07c1496396bb3ac7fca37539e7
dennee <imdennee@gmail.com>
2019-04-02 18:44:44 +0600

    add logs

--- src/lmp.UrlsProcessorWithProblem/Program.cs
+++ src/lmp.UrlsProcessorWithProblem/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
+using NLog.Config;
 
 namespace lmp.UrlsProcessorWithProblem
 {
@@ -10,6 +12,13 @@ namespace lmp.UrlsProcessorWithProblem
     {
         static void Main(string[] args)
         {
+            LogManager.Configuration = new XmlLoggingConfiguration("NLog.config");
+
+            var logger = LoggerFactory.Create<Program>();
+
+            logger.Info("Processor started");
+
+            Console.ReadKey();
         }
     }
 }
