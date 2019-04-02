715d2b4de9908e98c50ecc4974edd0bb1e0fa0b6
dennee <imdennee@gmail.com>
2019-04-02 20:24:58 +0600

    add logger creation by name

--- src/lmp.UrlsProcessorWithProblem/LoggerFactory.cs
+++ src/lmp.UrlsProcessorWithProblem/LoggerFactory.cs
@@ -11,9 +11,13 @@ namespace lmp.UrlsProcessorWithProblem
         /// Creates a logger
         /// </summary>
         /// <returns>A created logger</returns>
-        public static ILogger Create<T>()
-        {
-            return LogManager.GetLogger(typeof(T).FullName);
-        }
+        public static ILogger Create<T>() => Create(typeof(T).FullName);
+
+        /// <summary>
+        /// Create a logger
+        /// </summary>
+        /// <returns>A created logger</returns>
+        /// <param name="name">Name of logger</param>
+        public static ILogger Create(string name) => LogManager.GetLogger(name);
     }
 }
