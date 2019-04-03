e9e9f636b8136444c3252d0e1fc154bed7240dd5
dennee <imdennee@gmail.com>
2019-04-03 18:14:17 +0600

    add program starting

--- src/lmp.ProcessorWithProblem/Program.cs
+++ src/lmp.ProcessorWithProblem/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using lmp.UrlsProcessorWithProblem.FakeDataGenerator;
 using Ninject;
+using Ninject.Parameters;
 using NLog;
 using NLog.Config;
 
@@ -18,12 +22,70 @@ namespace lmp.UrlsProcessorWithProblem
             LogManager.Configuration = new XmlLoggingConfiguration("NLog.config");
 
             var logger = LoggerFactory.Create<Program>();
+            try
+            {
+                var count = int.Parse(GetArgValue(args, "count"));
 
-            _kernel = new StandardKernel(new IocModule());
+                _kernel = new StandardKernel(new IocModule());
 
-            logger.Info("Processor started");
+                var processorsManager = _kernel.Get<ProcessorsManager>(new ConstructorArgument("kernel", _kernel));
+                var dataGenerator = _kernel.Get<IFakeDataGenerator<string>>();
 
-            Console.ReadKey();
+                logger.Info("Processing started");
+
+                var data = dataGenerator.Generate(count);
+
+                processorsManager.UploadData(data);
+                processorsManager.Process();
+                logger.Info("Processing finished");
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                logger.Fatal(ex, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Gets an argument value from <paramref name="args"/>
+        /// </summary>
+        /// <param name="args">Arguments collection</param>
+        /// <param name="key">An argument key</param>
+        /// <returns>An argument value</returns>
+        private static string GetArgValue(string[] args, string key)
+        {
+            var parsedArgs = ParseArgs(args);
+
+            return parsedArgs[key];
+        }
+
+        /// <summary>
+        /// Parses arguments collection to dictionary
+        /// </summary>
+        /// <param name="args">Arguments collection</param>
+        /// <returns>Parsed arguments</returns>
+        private static IDictionary<string, string> ParseArgs(string[] args)
+        {
+            var parsedArgs = new ConcurrentDictionary<string, string>();
+
+            if (args.Length == 0)
+            {
+                throw new ArgumentException("The arguments collection is empty");
+            }
+
+            var argumentPattern = "--([a-z]+)=(.*?)(\\s|$)";
+
+            foreach (var arg in args)
+            {
+                if (Regex.IsMatch(arg, argumentPattern))
+                {
+                    var match = Regex.Match(arg, argumentPattern);
+                    parsedArgs.TryAdd(match.Groups[1].ToString(), match.Groups[2].ToString());
+                }
+            }
+
+            return parsedArgs;
         }
     }
 }
