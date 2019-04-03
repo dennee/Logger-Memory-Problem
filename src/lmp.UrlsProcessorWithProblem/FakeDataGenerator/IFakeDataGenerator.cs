5946324fefcc3c2d2a2b00ad7e9f62bfcf450e25
dennee <imdennee@gmail.com>
2019-04-03 16:51:37 +0600

    add fake data generator

---
+++ src/lmp.UrlsProcessorWithProblem/FakeDataGenerator/IFakeDataGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace lmp.UrlsProcessorWithProblem.FakeDataGenerator
+{
+    /// <summary>
+    /// Describes fake data generator
+    /// </summary>
+    /// <typeparam name="T">Type of generated data</typeparam>
+    public interface IFakeDataGenerator<T>
+    {
+        /// <summary>
+        /// Generates fake data collection
+        /// </summary>
+        /// <param name="count">Count of fake data items</param>
+        /// <returns>Generated data</returns>
+        ICollection<T> Generate(int count);
+    }
+}
